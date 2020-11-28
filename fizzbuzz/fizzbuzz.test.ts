0aa180a2200c454e2a44ef21cf77d2badc8e575d
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-11-28 21:09:33 +0100

    Tests more cases

--- fizzbuzz/fizzbuzz.test.ts
+++ fizzbuzz/fizzbuzz.test.ts
@@ -2,10 +2,14 @@ import {fizzbuzz} from "./index";
 
 describe('fizzbuzz', () => {
     it('should return "Fizz" when argument is a number multiple of three', () => {
-        expect(fizzbuzz(3)).toBe("Fizz");
+      [3, 6, 9, 12].forEach(number => {
+        expect(fizzbuzz(number)).toBe("Fizz");
+      });
     });
 
     it('should return "Buzz" if the number is a multiple of five', () => {
-        expect(fizzbuzz(5)).toBe("Buzz");
+      [5, 10].forEach(number => {
+        expect(fizzbuzz(number)).toBe("Buzz");
+      });
     });
 });
