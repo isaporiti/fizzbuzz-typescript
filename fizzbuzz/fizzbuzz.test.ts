e4b875fb42339f1af87c68d401a8ae00a7ee70d5
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-11-28 21:04:34 +0100

    Covers "multiple of 5" -> "Buzz" case

--- fizzbuzz/fizzbuzz.test.ts
+++ fizzbuzz/fizzbuzz.test.ts
@@ -4,4 +4,8 @@ describe('fizzbuzz', () => {
     it('should return "Fizz" when argument is a number multiple of three', () => {
         expect(fizzbuzz(3)).toBe("Fizz");
     });
+
+    it('should return "Buzz" if the number is a multiple of five', () => {
+        expect(fizzbuzz(5)).toBe("Buzz");
+    });
 });
--- fizzbuzz/index.ts
+++ fizzbuzz/index.ts
@@ -1,3 +1,6 @@
 export function fizzbuzz(number: number) {
+  if (number % 5 === 0) {
+    return "Buzz";
+  }
   return "Fizz";
 }
