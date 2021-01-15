7a375341ff61169318f499314ac4cae0f158673c
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2021-01-15 14:52:33 +0100

    Adds strict equals evaluation

--- fizzbuzz/fizzbuzz.ts
+++ fizzbuzz/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export function fizzbuzz(number: number) {
   const numberIsMultipleOfThree = number % 3 === 0;
-  const numberIsMultipleOfFive = number % 5 == 0;
+  const numberIsMultipleOfFive = number % 5 === 0;
 
   if (numberIsMultipleOfThree && numberIsMultipleOfFive) {
     return "FizzBuzz";
